515d669daa6f740c9ae0ba6eecfbd0f18d90328f
thiba <thibaultcourcol@gmail.com>
2024-04-19 00:56:57 +0200

    implementation de libgdx

--- src/main/java/l3/tpjeudelavie/JeuDeLaVieGame.java
+++ src/main/java/l3/tpjeudelavie/JeuDeLaVieGame.java
@@ -1,22 +1,2 @@
-package l3.tpjeudelavie;
-
-import com.badlogic.gdx.Game;
-
-public class JeuDeLaVieGame extends Game {
-    @Override
-    public void create() {
-
-    }
-
-    @Override
-    public void render() {
-        super.render();
-        // Logique de rendu du jeu
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-        // Nettoyage des ressources
-    }
+package l3.tpjeudelavie;public class JeuDeLaVieGame {
 }
--- src/main/java/l3/tpjeudelavie/JeuDeLaVieLaucher.java
+++ src/main/java/l3/tpjeudelavie/JeuDeLaVieLaucher.java
@@ -2,28 +2,35 @@ package l3.tpjeudelavie;
 
 import l3.tpjeudelavie.Observateur.ObservateurStats;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import javax.swing.*;
+import java.awt.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class JeuDeLaVieLaucher {
+
     public static void main(String[] args){
         JeuDeLaVie vie = new JeuDeLaVie(85, 85);
-        JeuDeLaVieGame vieGame = new JeuDeLaVieGame(vie);
+        JeuDeLaVieUI vieUI = new JeuDeLaVieUI(vie);
         ObservateurStats stats = new ObservateurStats(vie);
-        vie.attacheObservateur(vieGame);
+        vie.attacheObservateur(vieUI);
         vie.attacheObservateur(stats);
 
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1200;
-        config.height = 800;
-        config.resizable = true;
-        new LwjglApplication(vieGame, config);
+        JFrame frame = new JFrame("Jeu de la vie");
+        frame.setSize(1200, 800);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(vieUI, BorderLayout.CENTER);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+
+        vieUI.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
 
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
         executor.scheduleAtFixedRate(vie::calculerGenerationSuivante, 0, 70, TimeUnit.MILLISECONDS);
 
         vie.restartExecutor();
